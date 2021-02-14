baf1b03c58c0b5e245a83e9f347dbc4662a1ed66
Arifur Rahman <arifurrahman1299@gmail.com>
2021-02-14 13:38:51 +0600

    2nd task

---
+++ Lab task 2 14-02-21/Lab task 2 14-02-21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_task_2_14_02_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 10, 11, 11, 12, 12, 40, 40, 16, 17, 18 };
+            int i, j,  k= 0;
+
+
+            for (i = 0; i < 10; i++)
+            {
+                for (j = i + 1; j < 10; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        k++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n Total  duplicate elements found :" + k);
+            Console.ReadLine();
+        }
+    }
+}

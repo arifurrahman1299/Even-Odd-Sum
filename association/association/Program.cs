a09c608b95d188ef8634276682ec7b9559cc7b78
Arifur Rahman <arifurrahman1299@gmail.com>
2021-02-28 14:02:05 +0600

    task 4

---
+++ association/association/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Depertment d1 = new Depertment("Computer science", "CS");
+            Depertment d1 = new Depertment("Electrical ENGG", "EEE");
+            Student s1 = new student("Arifur Rahman", "18-38852-3", "3.5f");
+            Student s2 = new student("Arif", "18-38852-3", "2.5f");
+
+
+            d1.AddStudent(s1);
+            d2.AddStudent(s2);
+
+        }
+
+
+
+    }
+}
---
+++ association/association/student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace association
+{
+    class student
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private float Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+        public student() { }
+        public Student(string name, string id, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+        }
+        public void showinfo()
+        {
+            console.writeline("Name: " + name);
+            console.writeline("id: " + id);
+            console.writeline("cgpa: " + cgpa);
+
+        }
+
+    }
+}
